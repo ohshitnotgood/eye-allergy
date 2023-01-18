ba271781c5df2c6f1219d856c612f475470605cd
= <=>
2023-01-18 17:45:03 +0100

    Initial Commit

---
+++ Eye Health Tracker/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Eye Health Tracker
+//
+//  Created by Praanto on 2023-01-18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Eye Health Tracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Eye Health Tracker
+//
+//  Created by Praanto on 2023-01-18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
